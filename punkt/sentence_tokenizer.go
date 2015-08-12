3e7329790744a8067e91e447361fdf89643bf7bc
Eric Bower <neurosnap@gmail.com>
2015-08-12 18:33:12 -0400

    weve got symmetry!!!

--- punkt/sentence_tokenizer.go
+++ punkt/sentence_tokenizer.go
@@ -2,6 +2,7 @@ package punkt
 
 import (
 	"fmt"
+	//	"strings"
 )
 
 // A sentence tokenizer which uses an unsupervised algorithm to build a model
@@ -21,7 +22,16 @@ func NewSentenceTokenizer(trainedData *PunktParameters) *SentenceTokenizer {
 
 func (s *SentenceTokenizer) Tokenize(text string) []string {
 	//last_break := 0
-	matches := s.PunktLanguageVars.PeriodContext(text)
-	fmt.Println(matches)
-	return matches
+	re := s.PunktLanguageVars.RePeriodContext()
+	fmt.Println(re)
+	matches := re.FindAllStringSubmatchIndex(text, -1)
+
+	for _, match := range matches {
+		fmt.Println("context: ", text[match[0]:match[1]])
+		fmt.Println("next_tok: ", text[match[4]:match[5]])
+		fmt.Println("start: ", match[2])
+		fmt.Println("end: ", match[4])
+		fmt.Println("-------")
+	}
+	return []string{}
 }
