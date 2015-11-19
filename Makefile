4986a7c1366fddc772c66e37d9fe249699aa4014
Eric Bower <neurosnap@gmail.com>
2015-11-19 13:26:45 -0500

    cmd looking better

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
 BASE_DIR=$(shell echo $$GOPATH)/src/github.com/neurosnap/sentences
+
 VERSION_FILE=$(BASE_DIR)/VERSION
 CURRENT_VERSION=$(shell cat $(VERSION_FILE))
-MAJOR=$(word 1, $(subst ., , $(CURRENT_VERSION)))
-MINOR=$(word 2, $(subst ., , $(CURRENT_VERSION)))
-PATCH=$(word 3, $(subst ., , $(CURRENT_VERSION)))
-VER ?= $(MAJOR).$(MINOR).$(shell echo $$(($(PATCH)+1)))
+
+COMMITHASH=$(shell git rev-parse --short HEAD)
 
 .PHONY: english
 
@@ -12,12 +11,17 @@ test:
 	go test ./...
 
 build:
-	go build -ldflags "-X main.VERSION=$(CURRENT_VERSION)" ./cmd/sentences
+	go build -ldflags "-X main.VERSION=$(CURRENT_VERSION) -X main.COMMITHASH=$(COMMITHASH)" ./cmd/sentences
 
 install:
-	go install -ldflags "-X main.VERSION=$(CURRENT_VERSION)" ./cmd/sentences
+	go install -ldflags "-X main.VERSION=$(CURRENT_VERSION) -X main.COMMITHASH=$(COMMITHASH)" ./cmd/sentences
 
 bump:
+	MAJOR=$(word 1, $(subst ., , $(CURRENT_VERSION)))
+	MINOR=$(word 2, $(subst ., , $(CURRENT_VERSION)))
+	PATCH=$(word 3, $(subst ., , $(CURRENT_VERSION)))
+	VER ?= $(MAJOR).$(MINOR).$(shell echo $$(($(PATCH)+1)))
+
 	echo $(VER) > $(VERSION_FILE)
 
 # Compile language specific training data
--- cmd/sentences/main.go
+++ cmd/sentences/main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
+	"github.com/neurosnap/sentences/english"
 	"github.com/spf13/cobra"
 )
 
 var VERSION string
+var COMMITHASH string
+
+var ver bool
+var fname string
+var delim string
 
 var sentencesCmd = &cobra.Command{
 	Use:   "sentences",
 	Short: "Sentence tokenizer",
 	Long:  "A utility that will break up a blob of text into sentences.",
 	Run: func(cmd *cobra.Command, args []string) {
-		//var text []byte
-		fmt.Print("Hi there")
-		/*if len(args) > 0 {
-			text, _ = ioutil.ReadFile(args[0])
+		var text []byte
+
+		if ver {
+			fmt.Println(VERSION)
+			fmt.Println(COMMITHASH)
+			return
+		}
+
+		if fname != "" {
+			text, _ = ioutil.ReadFile(fname)
 		} else {
 			reader := bufio.NewReader(os.Stdin)
 			text, _ = ioutil.ReadAll(reader)
@@ -30,12 +46,18 @@ var sentencesCmd = &cobra.Command{
 		sentences := tokenizer.Tokenize(string(text))
 		for _, s := range sentences {
 			text := strings.Join(strings.Fields(s.Text), " ")
-			fmt.Print(text)
-		}*/
+
+			text = strings.Join([]string{text, delim}, "")
+			fmt.Printf("%s", text)
+		}
 	},
 }
 
 func main() {
+	sentencesCmd.Flags().BoolVarP(&ver, "version", "v", false, "Get current version of sentences")
+	sentencesCmd.Flags().StringVarP(&fname, "file", "f", "", "Read file as source input instead of stdin")
+	sentencesCmd.Flags().StringVarP(&delim, "delimiter", "d", "\n", "Delimiter used to demarcate sentence boundaries")
+
 	if err := sentencesCmd.Execute(); err != nil {
 		fmt.Print(err)
 	}
