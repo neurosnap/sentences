a28447858c3b3513530eef43a83319f8074073db
Eric Bower <neurosnap@gmail.com>
2015-11-27 16:31:07 -0500

    bugfix: overwriting previous xml files

--- perf/brown.py
+++ perf/brown.py
@@ -10,11 +10,14 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         raise IOError("Please provide an output file for text")
 
+    all_text = ""
+
     path = sys.argv[1]
     for filename in os.listdir(path):
         if not filename.endswith('.xml'):
             continue
 
+        print(filename)
         fname = os.path.join(path, filename)
         tree = ET.parse(fname)
 
@@ -29,9 +32,12 @@ if __name__ == '__main__':
         text = text.replace(" !", "!\n")
         text = text.replace(" ?", "?\n")
 
-        output = sys.argv[2]
-        with open(output, "w") as fp:
-            fp.write(text)
+        all_text = '\n'.join([all_text, text])
+
+    output = sys.argv[2]
+    with open(output, "w") as fp:
+        fp.write(all_text)
+
         #paragraphs = root.findall("context/p")
         #for para in paragraphs:
         #    sentences = para.findall("s")
